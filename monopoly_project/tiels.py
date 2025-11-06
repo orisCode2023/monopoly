014b8b38c2e87d3a8d1fc7470df3a0e38e235104
Ori <odmark2023@gmail.com>
2025-11-06 18:52:39 +0200

    fixing the classes followed by the code DRY

--- monopoly_project/tiels.py
+++ monopoly_project/tiels.py
@@ -15,9 +15,6 @@ class Tile:
 class Property(Tile):
     def __init__(self, name, tile_type, available, price_purch, rent_price, city):
         super().__init__(name, tile_type, available)
-        self.name = name
-        self.tile = tile_type
-        self.available = available
         self.price = price_purch
         self.rent = rent_price
         self.city = city
@@ -30,21 +27,13 @@ class Property(Tile):
 class Rail(Property):
     def __init__(self, name, tile_type, available, price_purch, rent):
         super().__init__(name, tile_type, price_purch, available, rent)
-        self.name = name
-        self.tile = tile_type
-        self.available = available
-        self.rent = rent
-
-
+    
 
 class Bonus(Tile):
     def __init__(self, name, tile_type, available, amount):
         super().__init__(name, tile_type, available)
         self.amount = amount
-        self.name = name
-        self.tile = tile_type
-        self.available = available
-
+        
     def __str__(self):
         return super().__str__(), f"Get: {self.amount}"
 
@@ -53,11 +42,7 @@ class Bonus(Tile):
 class Tax(Tile):
     def __init__(self, name, tile_type, available, amount):
         super().__init__(name, tile_type, available)
-        self.name = name
-        self.tile = tile_type
-        self.available = available
-        self.amount = amount
-
+        
     def __str__(self):
         return super().__str__(), f"Pay: {self.amount}"
 
@@ -66,14 +51,9 @@ class Tax(Tile):
 class Jail(Tile):
     def __init__(self, name, tile_type, available):
         super().__init__(name, tile_type, available)
-        self.name = name
-        self.tile = tile_type
-        self.available = available
-
+        
 
 class GoToJail(Tile):
     def __init__(self, name, tile_type, available):
         super().__init__(name, tile_type, available)
-        self.name = name
-        self.tile = tile_type
-        self.available = available
+       
