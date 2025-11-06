29d59d1191e06cce9ec1094173c91fe5123f5e7d
Ori <odmark2023@gmail.com>
2025-11-06 19:14:39 +0200

    fixing the creation of the board

--- monopoly_project/board.py
+++ monopoly_project/board.py
@@ -12,52 +12,51 @@ class Board:
           case "start":
             tile["name"] = Tile(tile["name"], tile["type"], False)
             self.board.append(tile["name"])
-            break
+            
         
           case "tax":
             tile["name"] = Tax(tile["name"], tile["type"], False, tile["amount"])
             self.board.append(tile["name"])
-            break
+            
           
           case "bonus":
             tile["name"] = Bonus(tile["name"], tile["type"], False, tile["amount"])
             self.board.append(tile["name"])
-            break
+            
            
           case "end":
             tile["name"] = Tile(tile["name"], tile["type"], False)
             self.board.append(tile["name"])
-            break
+            
            
           case "go_to_jail":
             tile["name"] = GoToJail(tile["name"], tile["type"], False)
             self.board.append(tile["name"])
-            break
+            
 
           case "jail":
             tile["name"] = Jail(tile["name"], tile["type"], False)
             self.board.append(tile["name"])
-            break
+            
 
           case "property":
             if "Rail" not in tile["name"]:
               tile["name"] = Property(tile["name"], tile["type"], True, tile["price"], tile["rent"], tile["city"])
               self.board.append(tile["name"])
-              break
+              
             else:
               tile["name"] = Rail(tile["name"], tile["type"], True, tile["price"], tile["rent"], tile["city"])
               self.board.append(tile["name"])
-              break
+              
           
     return self.board
 
      
   def check_boarders(self, current_location, steps_number):
-    location = current_location
-    if (current_location + steps_number) > len(self.board):
-        location = ((len(self.board) - current_location) + steps_number ) -1 
-    return location
-                       
+    return (current_location + steps_number) % len(self.board)
+  
+
+       
 
 
 
